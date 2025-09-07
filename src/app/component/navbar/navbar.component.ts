b7ac436b0636cc158e4cece0b864ad420ce7a9ff
Chetan Raghava <chetanr2512@gmail.com>
2025-09-07 11:29:04 +0530

    Navbar and Product-listing UI fixes

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+cartItemCount: Observable<number>;
+  isMobileMenuOpen = false;
 
+  constructor(private cartService: CartService) {
+    this.cartItemCount = this.cartService.getCartItemCount$();
+  }
+
+  ngOnInit(): void {
+    // Component initialization if needed
+  }
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
